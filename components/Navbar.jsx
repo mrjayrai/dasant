06ebdec82549d8b7bcd475fee58014e38fef6747
mrjayrai <raijai127@gmail.com>
2024-09-07 17:45:50 +0530

    added styling menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -59,7 +59,7 @@ export default function Navbar() {
     </div>
     <div className={`space-y-4 px-4 mt-32 py-14 bg-brandPrimary ${isMenuOpen? "block fixed top-0 right-0 left-0":"hidden"}`}>
     {navItems.map(({ link, path }) => (
-                <Link to={path} spy={true} smooth={true} key={path} offset={-100} className='block text-base text-gray900 hover:text-brandPrimary first:font-medium'>{link}</Link>
+                <Link to={path} spy={true} smooth={true} key={path} offset={-100} className='block text-base text-white hover:text-white first:font-medium'>{link}</Link>
            
         ))}
     </div>
