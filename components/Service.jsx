64a4ae5df129ede49ebe0c82eaa3724c42594109
mrjayrai <raijai127@gmail.com>
2024-09-10 10:58:14 +0530

    added clients

--- components/Service.jsx
+++ components/Service.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import clogo1 from "../src/assets/clogo1.jpeg";
+import clogo2 from "../src/assets/clogo2.jpeg";
+import clogo3 from "../src/assets/clogo3.jpeg";
+import clogo4 from "../src/assets/clogo4.jpeg";
+
 
 function Service() {
   return (
@@ -7,10 +12,10 @@ function Service() {
         <h2 className='text-4xl text-neutralDGrey font-semibold mb-2'>Our Clients</h2>
         <p className='text-neutralGray'>We have been working with some amazing clients</p>
         <div className='my-12 flex flex-wrap justify-between items-center gap-4 '>
-          <img src = "/src/assets/clogo1.jpeg" className='h-32 w-32 cursor-pointer' alt=''></img>
-          <img src = "/src/assets/clogo2.jpeg" className='h-32 w-32 cursor-pointer' alt=''></img>
-          <img src = "/src/assets/clogo3.jpeg" className='h-32 w-32 cursor-pointer' alt=''></img>
-          <img src = "/src/assets/clogo4.jpeg" className='h-32 w-32 cursor-pointer' alt=''></img>
+          <img src ={clogo1} className='h-32 w-32 cursor-pointer' alt=''></img>
+          <img src ={clogo2}  className='h-32 w-32 cursor-pointer' alt=''></img>
+          <img src = {clogo3} className='h-32 w-32 cursor-pointer' alt=''></img>
+          <img src ={clogo4} className='h-32 w-32 cursor-pointer' alt=''></img>
         </div>
       </div>
       <div className='text-center my-8'>
