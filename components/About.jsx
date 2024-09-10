c9b827f1af8db2557273cf6a6f19415fb0668408
mrjayrai <raijai127@gmail.com>
2024-09-10 20:22:48 +0530

    .

---
+++ components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import studiologo from "../src/assets/studiologo.png";
+
+function About() {
+  return (
+    <div>
+      <div className="text-center mt-10  mx-auto">
+        <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">
+          About Us
+        </h2>
+    </div>
+    </div>
+  )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Home from '../components/Home';
 import Service from '../components/Service';
 import ServiceDesc from '../components/ServiceDesc';
+import About from '../components/About';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <>
             <Home />
             <Service />
+            <About/>
           </>
         } />
         
