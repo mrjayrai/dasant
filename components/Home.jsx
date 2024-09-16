af74bf92aea74a8bb934d4b052306e4f896d2d94
mrjayrai <raijai127@gmail.com>
2024-09-16 18:25:50 +0530

    added banners

--- components/Home.jsx
+++ components/Home.jsx
@@ -16,11 +16,11 @@ export default function Home() {
           >
             Your browser does not support the video tag.
           </video>
-          <div className='absolute top-1/4  left-1/2 sm:left-1/4 md:left-1/4 lg:left-1/4 xl:left-1/4 transform -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-center'>
+          {/* <div className='absolute top-1/4  left-1/2 sm:left-1/4 md:left-1/4 lg:left-1/4 xl:left-1/4 transform -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-center'>
             <div className='text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center'>
               Welcome to Dashantee Studio
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
