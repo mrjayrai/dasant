0de0aaa23bf395775e7c4f78756fbc427a7817fe
mrjayrai <raijai127@gmail.com>
2024-09-16 18:40:03 +0530

    added banners

--- components/Home.jsx
+++ components/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className='bg-neutralSilver'>
       <div className='pt-16 relative'>
-        <div className='relative w-full h-[500px] md:h-[700px]'>
+        <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
           <video
-            className='w-full h-full object-contain'
+            className='absolute inset-0 w-full h-full object-cover'
             autoPlay
             loop
             muted
@@ -16,11 +16,6 @@ export default function Home() {
           >
             Your browser does not support the video tag.
           </video>
-          {/* <div className='absolute top-1/4  left-1/2 sm:left-1/4 md:left-1/4 lg:left-1/4 xl:left-1/4 transform -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-center'>
-            <div className='text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center'>
-              Welcome to Dashantee Studio
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
