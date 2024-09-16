1bb1ca62b7302282be0bc56d27e70c2a27afde99
mrjayrai <raijai127@gmail.com>
2024-09-16 17:49:23 +0530

    added banners

--- components/Service.jsx
+++ components/Service.jsx
@@ -82,7 +82,7 @@ function Service() {
   ];
   return (
     <div className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto">
-      <div className="text-center my-8">
+      {/* <div className="text-center my-8">
         <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">
           Our Clients
         </h2>
@@ -95,7 +95,37 @@ function Service() {
           <img src={clogo3} className="h-32 w-32 cursor-pointer" alt=""></img>
           <img src={clogo4} className="h-32 w-32 cursor-pointer" alt=""></img>
         </div>
-      </div>
+      </div> */}
+      <div className="text-center my-8">
+  <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">
+    Our Clients
+  </h2>
+  <p className="text-neutralGray">
+    We have been working with some amazing clients
+  </p>
+  <div className="relative overflow-hidden">
+    <div className="flex animate-scroll whitespace-nowrap pt-5">
+      <img src={clogo1} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo2} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo3} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo4} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo1} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo2} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo3} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo4} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo1} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo2} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo3} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo4} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo1} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo2} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo3} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+      <img src={clogo4} className="h-32 w-32 cursor-pointer inline-block mx-4 px-2" alt="" />
+    </div>
+  </div>
+  
+</div>
+
       <div className="text-center mt-20  mx-auto">
         <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">
           Our Services
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,15 @@ export default {
           950: '#172554',
         },
       },
+      animation: {
+        'scroll': 'scroll 18s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
     },
   },
   plugins: [
