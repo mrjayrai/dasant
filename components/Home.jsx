c8d7191cd77f4a4df0f617e4feabc626c3bdc4bc
mrjayrai <raijai127@gmail.com>
2024-10-12 16:16:04 +0530

    .

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,5 +1,29 @@
+// import React from 'react';
+// import dasant from "../src/assets/dasant.mp4";
+
+// export default function Home() {
+//   return (
+//     <div className='bg-neutralSilver'>
+//       <div className='pt-16 relative'>
+//         <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+//           <video
+//             className='absolute inset-0 w-full h-full object-cover'
+//             autoPlay
+//             loop
+//             muted
+//             playsInline
+//             src={dasant}
+//           >
+//             Your browser does not support the video tag.
+//           </video>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+////// desktop ^
 import React from 'react';
-import homeintro2 from "../src/assets/homeintro2.mp4";
 import dasant from "../src/assets/dasant.mp4";
 
 export default function Home() {
@@ -8,7 +32,7 @@ export default function Home() {
       <div className='pt-16 relative'>
         <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
           <video
-            className='absolute inset-0 w-full h-full object-cover'
+            className='absolute inset-0 w-full h-full object-contain'
             autoPlay
             loop
             muted
@@ -22,3 +46,4 @@ export default function Home() {
     </div>
   );
 }
+
