9f7f3d4a461723f677014c57bc4bf59f42fda33c
mrjayrai <raijai127@gmail.com>
2024-10-12 16:18:40 +0530

    /

--- components/Home.jsx
+++ components/Home.jsx
@@ -29,10 +29,10 @@ import dasant from "../src/assets/dasant.mp4";
 export default function Home() {
   return (
     <div className='bg-neutralSilver'>
-      <div className='pt-16 relative'>
-        <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+      <div className='pt-0 relative'>
+        <div className='relative w-full h-screen'>
           <video
-            className='absolute inset-0 w-full h-full object-contain'
+            className='absolute inset-0 w-full h-full object-cover'
             autoPlay
             loop
             muted
