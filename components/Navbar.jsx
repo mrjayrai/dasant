c42fc28bf4ac27fa4a74a12a46ab62cee7048ae2
mrjayrai <raijai127@gmail.com>
2024-10-13 23:32:35 +0530

    mc

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
       <header className='w-full bg-PrimaryColor  fixed top-0 left-0 right-0 z-[10000]'>
       <nav className={`py-1 lg:px-1 px-1 ${isSticky ? "sticky top-0 left-0 right-0 border-b-[1px] shadow border-b-slate-500 bg-PrimaryColor duration-75" : ""}`}>
         <div className='flex justify-between items-center text-lg gap-[2px] mr-10'>
-            <a href='/' className='flex'><img src={dashanteelogo} style={{ height: '100px', width: '120px',margin:'2px' }} alt=''/></a>
+            <a href='/' className='flex'><img src={dashanteelogo} style={{ height: '80px', width: '80px',margin:'2px' }} alt=''/></a>
             <ul className=' md:flex space-x-12 hidden'>
         {navItems.map(({ link, path }) => (
             <li key={path} className='cursor-pointer'>
--- components/Teams.jsx
+++ components/Teams.jsx
@@ -47,11 +47,11 @@ function Teams() {
     </div>
     {/* old end */}
     {/* new start */}
-    {/* <div className="p-10 max-w-screen-lg mx-auto">
+    <div className="p-10 max-w-screen-lg mx-auto">
     <div className="text-center mb-4">
-        <p className="mt-4 text-sm leading-7 text-gray-500 font-regular">
+        {/* <p className="mt-4 text-sm leading-7 text-gray-500 font-regular">
             THE TEAM
-        </p>
+        </p> */}
         <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">
             Our <span className="text-indigo-600">Team</span>
         </h3>
@@ -67,11 +67,11 @@ function Teams() {
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4">
                 <div>
                     <a href="#"
-                        className="text-gray-900 font-bold text-xl mb-2 hover:text-indigo-600 transition duration-500 ease-in-out">
-                        Yahya R. Makarim
+                        className="text-gray-900 font-bold text-xl mb-2 hover:text-indigo-600 transition duration-500 ease-in-out capitalize">
+                        {member.name}
                     </a>
                     <p className="text-sm text-gray-600">
-                        CEO &amp; President
+                        {member.desg}
                     </p>
                     <p className="text-gray-500 text-base mt-4">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -109,7 +109,7 @@ function Teams() {
                                 </g>
                             </svg>
                         </a>
-                        <a href="#">
+                        {/* <a href="#">
                             <svg width="19px" height="19px" viewBox="0 0 239.04 239.04">
                                 <g>
                                     <path style={{ fill: '#FFFFFF' }} d="M167.809,36.555c-22.276-0.183-44.597-1.054-66.919-0.504c-25.851,0.642-46.018,2.2-61.281,22.872
@@ -127,7 +127,31 @@ function Teams() {
                                         c1.299-3.123,4.168-3.484,4.168-3.484C163.727,49.594,157.588,48.38,157.588,48.38z"></path>
                                 </g>
                             </svg>
-                        </a>
+                        </a> */}
+
+<a href="#">
+                            <svg width="19px" height="19px" version="1.1" id="Layer_1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 239.04 239.04" style={{}}
+                                xml:space="preserve">
+                                    {/* "enable-background:new 0 0 239.04 239.04;" */}
+                                <g>
+                                    <path style={{ fill: '#FFFFFF' }} d="M167.809,36.555c-22.276-0.183-44.597-1.054-66.919-0.504c-25.851,0.642-46.018,2.2-61.281,22.872
+		c-3.988,9.854-6.004,21.359-6.967,33.368c-0.183,3.163-0.367,6.417-0.596,9.763c-0.825,20.396,0.55,41.435,0.092,58.118
+		c2.154,10.955,6.371,20.809,14.255,28.326c26.126,24.888,79.294,17.142,114.128,15.767c16.73-6.096,30.663-11.138,38.134-30.251
+		c5.913-15.034,4.95-34.193,5.363-50.326C204.797,90.502,206.264,44.714,167.809,36.555z"></path>
+                                    <path d="M157.588,48.38c44.505,5.729,31.443,70.219,30.205,101.478c-0.688,17.188-3.758,30.48-21.496,36.438
+		c-23.33,7.838-57.706,5.225-81.494,1.742c-15.905-2.292-28.372-7.059-32.772-23.559c-6.967-26.08-4.996-61.373-1.971-87.957
+		c0.092-0.504,0.229-0.871,0.458-1.192C50.93,35.547,134.441,45.401,157.588,48.38z"></path>
+                                    <path style={{ fill: '#FFFFFF' }} d="M176.059,72.627c0.092-10.955-13.567-14.438-20.901-7.7c-1.971,1.833-2.888,4.629-3.025,7.654
+		c0.733,3.896,3.071,7.746,6.096,9.946C165.334,87.523,175.967,80.877,176.059,72.627z"></path>
+                                    <path style={{ fill: '#FFFFFF' }}
+                                        d="M155.754,93.94c-15.675-23.742-51.06-26.08-70.081-5.042c-12.971,14.346-13.888,38.226-4.125,55.185
+		c0.55,0.183,1.054,0.504,1.513,1.054c15.859,17.692,43.176,24.109,62.794,8.067C163.363,138.858,168.496,113.236,155.754,93.94z"></path>
+                                    <path d="M93.282,126.483c5.546,15.584,25.255,27.959,40.976,18.334c31.763-19.434-5.546-72.235-31.718-48.814
+		C93.282,101.457,89.661,116.261,93.282,126.483z"></path>
+                                </g>
+                            </svg></a>
                     </div>
                 </div>
             </div>
@@ -135,7 +159,7 @@ function Teams() {
 ))}
         
     </div>
-</div> */}
+</div>
 
     {/* new end */}
     </>
