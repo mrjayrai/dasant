7e3e02d102a0769fe1000580bbe8275caaf11aa5
mrjayrai <raijai127@gmail.com>
2024-10-13 13:00:52 +0530

    .

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import dasant from "../src/assets/dasant.mp4";
+// import React from 'react';
+// import dasant from "../src/assets/dasant.mp4";
 
-export default function Home() {
-  return (
-    <div className='bg-neutralSilver'>
-      <div className='pt-16 relative'>
-        <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
-          <video
-            className='absolute inset-0 w-full h-full object-cover'
-            autoPlay
-            loop
-            muted
-            playsInline
-            src={dasant}
-          >
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      </div>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div className='bg-neutralSilver'>
+//       <div className='pt-16 relative'>
+//         <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+//           <video
+//             className='absolute inset-0 w-full h-full object-cover'
+//             autoPlay
+//             loop
+//             muted
+//             playsInline
+//             src={dasant}
+//           >
+//             Your browser does not support the video tag.
+//           </video>
+//         </div>
+//       </div>
+//     </div>
+    
+//   );
+// }
 
 ////// desktop ^
 // import React from 'react';
@@ -65,3 +66,45 @@ export default function Home() {
 //     </>
 //   );
 // }
+
+import React from 'react';
+import dasant from "../src/assets/dasant.mp4";
+import dasantmob from '../src/assets/dasantmob.mp4';
+export default function Home() {
+  return (
+    <>
+    <div className='bg-neutralSilver sm:hidden'>
+      <div className='pt-16 relative'>
+        <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+          <video
+            className='absolute inset-0 w-full h-full object-cover'
+            autoPlay
+            loop
+            muted
+            playsInline
+            src={dasant}
+          >
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    </div>
+    <div className='bg-neutralSilver max-sm:hidden'>
+      <div className='pt-16 relative'>
+        <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+          <video
+            className='absolute inset-0 w-full h-full object-cover'
+            autoPlay
+            loop
+            muted
+            playsInline
+            src={dasantmob}
+          >
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
