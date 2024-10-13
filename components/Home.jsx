9cfa6a6e2842ee9599fee853f799bb877d5013e6
mrjayrai <raijai127@gmail.com>
2024-10-13 13:38:56 +0530

    .

--- components/Home.jsx
+++ components/Home.jsx
@@ -31,11 +31,11 @@ import dasantmob from '../src/assets/dasantmob.mp4';
 export default function Home() {
   return (
     <>
-    <div className='bg-neutralSilver relative sm:hidden' style={{ transformStyle: 'preserve-3d', WebkitTransformStyle: 'preserve-3d' }}>
+    <div className='bg-neutralSilver relative z-0 sm:hidden' style={{ transformStyle: 'preserve-3d', WebkitTransformStyle: 'preserve-3d' }}>
       <div className='relative w-full'> {/* Changed to h-screen for full height */}
       {/* h-screen overflow-hidden */}
         <video
-          className='absolute inset-0 w-full h-fit object-contain sm:object-cover z-0' // Use object-contain for smaller screens, object-cover for larger screens
+          className='absolute inset-0 w-full h-fit object-contain sm:object-cover ' // Use object-contain for smaller screens, object-cover for larger screens
           autoPlay
           loop
           muted
