f9030e51c292fbb27adef6992c7a94e88918fdf1
mrjayrai <raijai127@gmail.com>
2024-10-13 14:19:01 +0530

    .

--- components/Videomob.jsx
+++ components/Videomob.jsx
@@ -4,7 +4,7 @@ import dasant from '../src/assets/dasant.mp4';
 export default function Videomob() {
   return (
     <div className='bg-neutralSilver relative z-0 sm:hidden' style={{ transformStyle: 'preserve-3d', WebkitTransformStyle: 'preserve-3d' }}>
-      <div className='relative w-full'> {/* Changed to h-screen for full height */}
+      <div className=' pt-16 relative w-full'> {/* Changed to h-screen for full height */}
       {/* h-screen overflow-hidden */}
         <video
           className='absolute inset-0 w-full h-fit object-contain sm:object-cover ' // Use object-contain for smaller screens, object-cover for larger screens
