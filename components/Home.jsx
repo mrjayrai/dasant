6b2c7a38094dd2a93c92aec00b26efa843d6d73b
mrjayrai <raijai127@gmail.com>
2024-10-13 12:51:31 +0530

    .

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,55 +1,12 @@
-// import React from 'react';
-// import dasant from "../src/assets/dasant.mp4";
-
-// export default function Home() {
-//   return (
-//     <div className='bg-neutralSilver'>
-//       <div className='pt-16 relative'>
-//         <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
-//           <video
-//             className='absolute inset-0 w-full h-full object-cover'
-//             autoPlay
-//             loop
-//             muted
-//             playsInline
-//             src={dasant}
-//           >
-//             Your browser does not support the video tag.
-//           </video>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-////// desktop ^
 import React from 'react';
 import dasant from "../src/assets/dasant.mp4";
-import dasantmob from '../src/assets/dasantmob.mp4';
 
 export default function Home() {
   return (
-    <>
-    <div className='bg-neutralSilver relative sm:hidden'>
-      <div className='relative w-full mb-16'> {/* Changed to h-screen for full height */}
-      {/* h-screen overflow-hidden */}
-        <video
-          className='absolute inset-0 w-full h-fit object-contain sm:object-cover z-0' // Use object-contain for smaller screens, object-cover for larger screens
-          autoPlay
-          loop
-          muted
-          playsInline
-          preload='auto'
-          src={dasantmob}
-        >
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </div>
-         <div className='bg-neutralSilver max-sm:hidden'>
-       <div className='pt-16 relative'>
-         <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
-         <video
+    <div className='bg-neutralSilver'>
+      <div className='pt-16 relative'>
+        <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+          <video
             className='absolute inset-0 w-full h-full object-cover'
             autoPlay
             loop
@@ -62,6 +19,49 @@ export default function Home() {
         </div>
       </div>
     </div>
-    </>
   );
 }
+
+////// desktop ^
+// import React from 'react';
+// import dasant from "../src/assets/dasant.mp4";
+// import dasantmob from '../src/assets/dasantmob.mp4';
+
+// export default function Home() {
+//   return (
+//     <>
+//     <div className='bg-neutralSilver relative sm:hidden'>
+//       <div className='relative w-full mb-16'> {/* Changed to h-screen for full height */}
+//       {/* h-screen overflow-hidden */}
+//         <video
+//           className='absolute inset-0 w-full h-fit object-contain sm:object-cover z-0' // Use object-contain for smaller screens, object-cover for larger screens
+//           autoPlay
+//           loop
+//           muted
+//           playsInline
+//           preload='auto'
+//           src={dasantmob}
+//         >
+//           Your browser does not support the video tag.
+//         </video>
+//       </div>
+//     </div>
+//          <div className='bg-neutralSilver max-sm:hidden'>
+//        <div className='pt-16 relative'>
+//          <div className='relative w-full h-[calc(100vh-4rem)] overflow-hidden'>
+//          <video
+//             className='absolute inset-0 w-full h-full object-cover'
+//             autoPlay
+//             loop
+//             muted
+//             playsInline
+//             src={dasant}
+//           >
+//             Your browser does not support the video tag.
+//           </video>
+//         </div>
+//       </div>
+//     </div>
+//     </>
+//   );
+// }
